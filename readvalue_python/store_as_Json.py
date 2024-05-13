aa6c766409ce24258d14264963697b1935d24d5d
Ian Lin <a0952386638@gmail.com>
2024-05-14 01:12:27 +0800

    Update store_as_Json.py

--- readvalue_python/store_as_Json.py
+++ readvalue_python/store_as_Json.py
@@ -1,6 +1,8 @@
 //pip3 install jsons
 import json
+//value of temperature is in value.py
 json_temp=json.dumps(temperature)
+//value of huminity is in value.py
 json_hum=json.dumps(huminity)
 txt='Temp:'+json_temp+'C/Humidity:'+json_hum
 with open('data.json','w') as obj:
